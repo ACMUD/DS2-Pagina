42083d1e5d6bb50ecc0f613641d4a777d4fd4a88
Michael-Jimenez-C <michaeljimenezcontreras1@gmail.com>
2025-03-18 17:21:03 -0500

    navigation

--- src/main.py
+++ src/main.py
@@ -2,6 +2,15 @@ import streamlit as st
 
 from pages import Introduction, Modelos
 
-pg = st.navigation([Introduction, Modelos])
+
+pages = {
+    "Introduccion": [
+        st.Page(Introduction, title="Introducción")
+    ],
+    "Modelos Matemáticos": [
+        st.Page(Modelos, title="Modelos matemáticos"),
+    ]
+}
+pg = st.navigation(pages)
 
 pg.run()
