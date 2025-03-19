c0d1fb2833a74283c3a40e950bf642c8e22869b3
Michael-Jimenez-C <michaeljimenezcontreras1@gmail.com>
2025-03-19 14:43:46 -0500

    páginas mongodb

--- src/pages/mongodb/mongo_python.py
+++ src/pages/mongodb/mongo_python.py
@@ -3,7 +3,7 @@ import streamlit as st
 def MongoPython():
     st.title("Mongo con Python")
     st.write("""
-             Para manejar cualquier base de datos se suele usar herramientas como ORM, en el caso de mongo ODM. sin embargo,
+             Para manejar cualquier base de datos se suele usar herramientas como un ORM que es una forma de representar la base de datos mediante objetos, en el caso de mongo ODM. sin embargo,
              primero se puede ver como se hace directamente utilizando la liberia correspondiente.
              """)
     st.header("Pymongo")
@@ -32,4 +32,27 @@ def MongoPython():
     
     st.header("Motor (asincrono)")
     st.link_button("Motor", "https://www.mongodb.com/docs/drivers/motor/",icon=":material/open_in_new:")
+    st.code("pip install motor")
+    st.write("motor es una libreria muy util para trabajar con mongo cuando se requiere un procesamiento asincrona y masiva de datos. A continuación un ejemplo de como se uso")
+    st.code("""
+            import asyncio
+            from motor.motor_asyncio import AsyncIOMotorClient
+
+            async def main():
+                uri = "mongodb://localhost:27017/"
+                client = AsyncIOMotorClient(uri)
+                db = client['test']
+                await db.users.insert_one({
+                    "nombre":"carlos",
+                    "email":"carlos@gmail.com"
+                    })
+
+            asyncio.run(main())
+            """)
     
+    st.markdown("""
+                Motor es util cuando:
+                *   Se tienen muchos clientes en una base de datos.
+                *   Se requiere un acceso masivo a los datos, por ejemplo una API.
+                *   Se requiere mayor fluidez en el procesamiento de los datos.
+                """)
--- src/pages/mongodb/odm.py
+++ src/pages/mongodb/odm.py
@@ -2,5 +2,76 @@ import streamlit as st
 def ODM():
     st.title("ODM (Object Document Mapper)")
     st.link_button("odmantic", "https://art049.github.io/odmantic/",icon=":material/open_in_new:")
+    st.markdown("""
+                ODMantic es un ORM (Object Document Mapper), que facilita la interacción y las consultas con la base de datos, permitiendo gestionar en código no solo los modelos y los tipos de datos sino tambien indices y propiedades de cada colección.
+                Los modelos de odmantic se basan en pydantic por lo cual facilitan mucho la validación de los datos y el procesmiento.
+                """)
     st.header("Modelado de la base de datos")
+    st.code("""
+            from odmantic import Model
+            
+            class User(Model):
+                name: str
+                email: str
+            """)
+    st.write("Luego se puede agregar una validación al modelo o incluso modelos anidados")
+    st.code("""
+            from odmantic import Field, Index, EmbeddedModel
+            
+            class Favoritos(EmbeddedModel):
+                musica: list[str] = []
+                peliculas: list[str] = []
+            
+            class User(Model):
+                name: str = Field(index=True)
+                email: str = Field(unique=True)
+                favoritos: Favoritos = Field(default_factory=Favoritos)
+                
+                model_config = {
+                    "Indexes": lambda: [
+                        Index("name"),
+                        Index("email", unique=True)
+                    ]
+                }
+            """)
+    
+    st.write("Todos los modelos tienen representaciones en json, y normalmente reciben otro objeto o un json")
+    
+    col1, col2 = st.columns(2)
+    col1.code("""
+            class SimpleUser(Model):
+                name: str = Field(index=True)
+                email: str = Field(unique=True)
+            
+            SimpleUser(user) # user -> User
+               """)
+    col2.json("""
+              {
+                  "name":"Juan",
+                  "email":"juan@gmail.com"
+              }
+              """)
+    
+    st.header("configuración de la base de datos")
+    st.write("Se puede construir una base de datos a partir de un modelo")
+    st.code("""
+            from odmantic import AIOEngine
+            from .models import User
+            
+            client = <cliente>
+            engine = AIOEngine(client, database = 'test')
+            
+            engine.configure_database([User])
+            """)
     st.header("Consultas con odmantic")
+    
+    st.write("Finalmente para buscar con ODMantic se puede usar el engine seguir del uso de las clases.")
+    st.write("Hay que tener en cuenta que ``ObjectId`` no puede ser serializado como json, por lo cual debe convertirse a ``str`` antes de serializarlo")
+    st.code("""
+             # Si es asincrono debe usarse con await dentro de una función asincrona
+             user = engine.find_one(User, User.email == 'juan@gmail.com')
+             
+             user.name = user.name.upper()
+             
+             engine.save(user)
+             """)
